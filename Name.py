fe776b6dcb918622aa24bac076c6b1a7f371f2a8
LantanSigni <63309066+LantanSigni@users.noreply.github.com>
2023-10-03 06:04:07 -0500

    fourth commit

---
+++ Name.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print(f"Hello {name}")
+
+age = input("How old are you? ")
+
+if int(age) >= 18:
+    print("You are adult already.")
+else:
+    print("You are infant yet.")
--- main.py
+++ main.py
@@ -7,3 +7,5 @@ joined_string = s1 + s2
 x = 8**3 + 4*(2 + 2)  # need to print a result
 print(f"{joined_string}")
 print(f"{x}")
+a = input("Invite: ")
+print(f"{a}")
