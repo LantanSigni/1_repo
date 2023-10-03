f1da082ce9a4d9045f3361a604bc1103e22ac2ab
LantanSigni <63309066+LantanSigni@users.noreply.github.com>
2023-10-03 05:55:05 -0500

    third commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Hello World")
-
 print("Hello Git")
+s = "Hello, World!"
+s1 = "Hello,"
+s2 = " World"
+joined_string = s1 + s2
+x = 8**3 + 4*(2 + 2)  # need to print a result
+print(f"{joined_string}")
+print(f"{x}")
