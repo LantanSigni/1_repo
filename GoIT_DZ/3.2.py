71a15e0f409615861cf990a26695cc33b4911350
LantanSigni <63309066+LantanSigni@users.noreply.github.com>
2023-10-24 11:35:55 -0500

    Added GoIT_DZ

--- GoIT_DZ/3.2.py
+++ GoIT_DZ/3.2.py
@@ -1,8 +1,9 @@
-username = input("Введіть ім'я: ")
-
-
 def invite_to_event(username):
-    print(f"Dear {username}, we have the honour to invite you to our event")
+    if username:
+        print(f"Dear {username}, we have the honour to invite you to our event.")
+    else:
+        print("Username cannot be empty. Please try again.")
 
 
+username = input("Введіть ім'я: ")
 invite_to_event(username)
