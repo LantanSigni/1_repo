f51c478b5a88e30889fc1fe5f9759e11c659bfc1
LantanSigni <63309066+LantanSigni@users.noreply.github.com>
2024-01-31 09:07:19 -0600

    GoIT_DZ

--- GoIT_DZ/SSec/13_user_class.py
+++ GoIT_DZ/SSec/13_user_class.py
@@ -7,6 +7,15 @@ class User:
         print(f'Hi, my login is {self.login}.')
 
 
+if __name__ == '__main__':  # перевірка класу User.
+    alice = User()
+    alice.greet()  # Hi, my login is Guest
+
+    bob = User()
+    bob.login = "Bob"
+    bob.greet()  # Hi, my login is Bob
+
+
 class AuthenticatedUser(User):
     '''Клас зареєстрованого користувача, який наслідує логін від Юзера та має свій атрибут пароля.
     Метод автентифікувати присвоює новий пароль зареєстрованому користувачу.'''
@@ -20,8 +29,26 @@ class AuthenticatedUser(User):
             return False
 
 
+if __name__ == '__main__':  # перевірка класу AuthenticatedUser.
+
+    alice = AuthenticatedUser()
+    alice.greet()  # Hi, my login is Guest
+    is_alice = alice.authenticate("password")
+    # Is 'alice' a default AuthenticatedUser: True
+    print(f"Is 'alice' a default AuthenticatedUser: {is_alice}")
+
+    bob = AuthenticatedUser()
+    bob.login = "Bob"
+    bob.password = "uNc1e"
+    bob.greet()  # Hi, my login is Bob
+    is_bob = bob.authenticate("password")
+    # Is 'bob' a default AuthenticatedUser: False
+    print(f"Is 'bob' a default AuthenticatedUser: {is_bob}")
+
+
 class AccessObject:
-    '''Клас об'єкту, до якого надається доступ, зі своїм вмістом, назвою цього об'єкта та вказуванням поточного власника.'''
+    '''Клас об'єкту, до якого надається доступ, зі своїм вмістом,
+    назвою цього об'єкта та вказуванням поточного власника.'''
     content = ''
     name = ''
     owner = User()
@@ -31,7 +58,9 @@ class AccessObject:
         Проводить перевірку належності обох користувачів до зареєстрованих користувачів та
         співпадіння вказаного паролю до паролю попереднього власника доступу.
         При проходженні всіх перевірок, назначається новий власник прав доступу'''
-        if isinstance(new_owner, AuthenticatedUser) and isinstance(self.owner, AuthenticatedUser) and old_owner_password == self.owner.password:
+        if (isinstance(new_owner, AuthenticatedUser) and
+            isinstance(self.owner, AuthenticatedUser) and
+                old_owner_password == self.owner.password):
             self.owner = new_owner
             print(f'The ownership changing of {
                 self.name} was successful! New owner is {self.owner.login}.')
@@ -40,27 +69,7 @@ class AccessObject:
                   self.name} owner changing detected!.')
 
 
-if __name__ == '__main__':  # перевірка коду.
-    # alice = User()
-    # alice.greet()  # Hi, my login is Guest
-
-    # bob = User()
-    # bob.login = "Bob"
-    # bob.greet()  # Hi, my login is Bob
-
-    # alice = AuthenticatedUser()
-    # alice.greet()  # Hi, my login is Guest
-    # is_alice = alice.authenticate("password")
-    # # Is 'alice' a default AuthenticatedUser: True
-    # print(f"Is 'alice' a default AuthenticatedUser: {is_alice}")
-
-    # bob = AuthenticatedUser()
-    # bob.login = "Bob"
-    # bob.password = "uNc1e"
-    # bob.greet()  # Hi, my login is Bob
-    # is_bob = bob.authenticate("password")
-    # # Is 'bob' a default AuthenticatedUser: False
-    # print(f"Is 'bob' a default AuthenticatedUser: {is_bob}")
+if __name__ == '__main__':  # Перевірка класу AccessObject.
 
     alice = AuthenticatedUser()
     alice.login = "Alice"
